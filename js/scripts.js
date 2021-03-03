f61af4f8b02144c9dd63de7168d912c93d7d5972
Wairimu Karuga <wairimukaruga4@gmail.com>
2021-03-03 05:08:01 +0300

    add the page's business logic

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Business logic
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+// user interface logic
+// $(document).ready(function() {
+
+//   $("form#new-contact").submit(function(event) {
+//     event.preventDefault();
+
+
+//   })
+// })
